10291039fbee6667ff9b9da791ab2a72aca43452
rspsoares <rspsoares@hotmail.com>
2016-07-12 19:00:35 -0300

    Primeiro commit

---
+++ AmericanView/AmericanView.WebSite/App_Start/BundleConfig.cs
@@ -0,0 +1,46 @@
+﻿using System.Web;
+using System.Web.Optimization;
+
+namespace AmericanView.WebSite
+{
+    public class BundleConfig
+    {      
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            /* -- SCRIPTS -- */
+            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+                  "~/Content/jquery/jquery-{version}.js"));                           
+            
+            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
+                    "~/Content/bootstrap/js/bootstrap.js",
+                    "~/Content/bootstrap/js/bootstrap-switch.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/jqueryui/js").Include(
+                      "~/Content/plugins/jquery-ui/jquery-ui.js"));
+          
+            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
+                    "~/Content/modernizr/modernizr-*"));
+
+            bundles.Add(new ScriptBundle("~/bundles/owlcarousel").Include(
+                "~/Content/plugins/owl.carousel/owl-carousel/owl.carousel.min.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/sticky").Include(
+                "~/Content/plugins/sticky/jquery.sticky.js"));
+
+
+            /* -- CSS -- */
+            bundles.Add(new StyleBundle("~/Content/css").Include(
+                "~/Content/kendo-ui/styles/web/kendo.common.css",
+                "~/Content/kendo-ui/styles/web/kendo.metro.css",
+                "~/Content/kendo-ui/styles/mobile/kendo.mobile.all.css",
+                "~/Content/bootstrap/css/bootstrap.css",
+                "~/Content/bootstrap/css/bootstrap-switch.css",
+                "~/Content/bootstrap/css/mosaic-custom-bootstrap.css",
+                "~/Content/integrador-default.css",
+                "~/Content/plugins/owl.carousel/owl-carousel/owl.carousel.css",
+                "~/Content/plugins/owl.carousel/owl-carousel/owl.theme.css"
+                
+                ));
+        }
+    }
+}
